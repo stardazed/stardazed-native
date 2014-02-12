58c3c1ab282184606087822df00f5025f510e092
Arthur Langereis <puurtuur@me.com>
2014-02-13 00:28:46 +0100

    TVec4 methods

--- include/Vector.hpp
+++ include/Vector.hpp
@@ -45,6 +45,18 @@ operator+ (const TVec3<T> lhs, const S rhs) {
 	return {{ lhs[0] + rhs, lhs[1] + rhs, lhs[2] + rhs }};
 }
 
+template <typename T>
+constexpr TVec4<T> operator+ (const TVec4<T> lhs, const TVec4<T> rhs) {
+	return {{ lhs[0] + rhs[0], lhs[1] + rhs[1], lhs[2] + rhs[2], lhs[3] + rhs[3] }};
+}
+
+template <typename T, typename S>
+constexpr
+std::enable_if_t<std::is_convertible<S, T>::value, TVec4<T>>
+operator+ (const TVec4<T> lhs, const S rhs) {
+	return {{ lhs[0] + rhs, lhs[1] + rhs, lhs[2] + rhs, lhs[3] + rhs }};
+}
+
 
 // ---- component-wise subtraction by other vec or scalar
 template <typename T>
@@ -57,6 +69,18 @@ constexpr
 std::enable_if_t<std::is_convertible<S, T>::value, TVec3<T>>
 operator- (const TVec3<T> lhs, const S rhs) {
 	return {{ lhs[0] - rhs, lhs[1] - rhs, lhs[2] - rhs }};
+}
+
+template <typename T>
+constexpr TVec4<T> operator- (const TVec4<T> lhs, const TVec4<T> rhs) {
+	return {{ lhs[0] - rhs[0], lhs[1] - rhs[1], lhs[2] - rhs[2], lhs[3] - rhs[3] }};
+}
+
+template <typename T, typename S>
+constexpr
+std::enable_if_t<std::is_convertible<S, T>::value, TVec4<T>>
+operator- (const TVec4<T> lhs, const S rhs) {
+	return {{ lhs[0] - rhs, lhs[1] - rhs, lhs[2] - rhs, lhs[3] - rhs }};
 }
 
 
@@ -73,6 +97,18 @@ operator* (const TVec3<T> lhs, const S rhs) {
 	return {{ lhs[0] * rhs, lhs[1] * rhs, lhs[2] * rhs }};
 }
 
+template <typename T>
+constexpr TVec4<T> operator* (const TVec4<T> lhs, const TVec4<T> rhs) {
+	return {{ lhs[0] * rhs[0], lhs[1] * rhs[1], lhs[2] * rhs[2], lhs[3] * rhs[3] }};
+}
+
+template <typename T, typename S>
+constexpr
+std::enable_if_t<std::is_convertible<S, T>::value, TVec4<T>>
+operator* (const TVec4<T> lhs, const S rhs) {
+	return {{ lhs[0] * rhs, lhs[1] * rhs, lhs[2] * rhs, lhs[3] * rhs }};
+}
+
 
 // ---- component-wise division by other vec or scalar
 template <typename T>
@@ -87,11 +123,28 @@ operator/ (const TVec3<T> lhs, const S rhs) {
 	return {{ lhs[0] / rhs, lhs[1] / rhs, lhs[2] / rhs }};
 }
 
+template <typename T>
+constexpr TVec4<T> operator/ (const TVec4<T> lhs, const TVec4<T> rhs) {
+	return {{ lhs[0] / rhs[0], lhs[1] / rhs[1], lhs[2] / rhs[2], lhs[3] / rhs[3] }};
+}
+
+template <typename T, typename S>
+constexpr
+std::enable_if_t<std::is_convertible<S, T>::value, TVec4<T>>
+operator/ (const TVec4<T> lhs, const S rhs) {
+	return {{ lhs[0] / rhs, lhs[1] / rhs, lhs[2] / rhs, lhs[3] / rhs }};
+}
+
 
 // ---- easily print vecs
 template <typename T>
 std::ostream& operator<< (std::ostream& os, const TVec3<T>& vec) {
 	return os << '(' << vec[0] << ',' << vec[1] << ',' << vec[2] << ')';
+}
+
+template <typename T>
+std::ostream& operator<< (std::ostream& os, const TVec4<T>& vec) {
+	return os << '(' << vec[0] << ',' << vec[1] << ',' << vec[2] << ',' << vec[3] << ')';
 }
 
 
