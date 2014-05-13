927143bd95ca23216c1bdb3b2e46ee96161536ec
Arthur Langereis <puurtuur@me.com>
2014-05-13 12:57:00 +0200

    time namespace provides typed double precision second-resolution time points

--- src/system/Time.hpp
+++ src/system/Time.hpp
@@ -12,11 +12,26 @@
 namespace stardazed {
 namespace time {
 
-	using Clock = std::chrono::high_resolution_clock;
-	using Point = Clock::time_point;
+	// The time namespace basically wraps std::chrono specializing on the high_resolution_clock
+	// and standardizing on a double precion seconds time value type.
 
-	inline Point now() { return Clock::now(); }
+	using Clock    = std::chrono::high_resolution_clock;
+	using Duration = std::chrono::duration<double, std::chrono::seconds::period>;
+	static_assert(std::chrono::treat_as_floating_point<Duration::rep>::value, "Seconds duration could not be treated as a double");
 
+	inline Duration now() { return Clock::now().time_since_epoch(); }
+	inline constexpr Duration zero() { return Duration::zero(); }
+
+	// create arbitrary Durations of a specified length to aid in clarity
+	inline constexpr Duration nanoseconds(double ns)  { return std::chrono::duration<double, std::chrono::nanoseconds::period>{ ns }; }
+	inline constexpr Duration microseconds(double us) { return std::chrono::duration<double, std::chrono::microseconds::period>{ us }; }
+	inline constexpr Duration milliseconds(double ms) { return std::chrono::duration<double, std::chrono::milliseconds::period>{ ms }; }
+	inline constexpr Duration seconds(double s)       { return std::chrono::duration<double, std::chrono::seconds::period>{ s }; }
+	inline constexpr Duration minutes(double m)       { return std::chrono::duration<double, std::chrono::minutes::period>{ m }; }
+	inline constexpr Duration hours(double h)         { return std::chrono::duration<double, std::chrono::hours::period>{ h }; }
+	
+	inline constexpr Duration hertz(double hz)        { return Duration{ 1. / hz }; }
+	
 } // ns time
 } // ns stardazed
 
