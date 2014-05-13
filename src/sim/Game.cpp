3084dbd0d8bf43660cd80e9b0c06827fe2414130
Arthur Langereis <puurtuur@me.com>
2014-05-13 13:17:10 +0200

    Game loop skeleton using time types

--- src/sim/Game.cpp
+++ src/sim/Game.cpp
@@ -9,8 +9,49 @@ namespace stardazed {
 
 
 class Game {
+public:
+	Game();
 	
+	void mainLoop();
 };
+
+
+Game::Game() {
+}
+
+
+void Game::mainLoop() {
+	auto previousTime = time::now();
+	auto simulationLag = time::zero(), renderLag = time::zero();
+	
+	const auto simulationFrameTime = time::hertz(120);
+	const auto renderFrameTime = time::hertz(60);
+	
+	while (1) {
+		auto currentTime = time::now();
+		auto elapsedTime = currentTime - previousTime;
+		previousTime = currentTime;
+		
+		simulationLag += elapsedTime;
+		renderLag += elapsedTime;
+
+		// process input
+		
+		while (simulationLag >= simulationFrameTime) {
+			// update physics, AI, etc.
+
+			simulationLag -= simulationFrameTime;
+		}
+		
+		if (renderLag >= renderFrameTime) {
+			// render frame
+			
+			// render single frame, drop any missed ones
+			while (renderLag >= renderFrameTime)
+				renderLag -= renderFrameTime;
+		}
+	}
+}
 	
 	
 } // ns stardazed
