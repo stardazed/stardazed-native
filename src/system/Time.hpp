bbf0598414536e70963104e1e36fd6fb0098b121
Arthur Langereis <puurtuur@me.com>
2014-05-31 11:42:13 +0200

    constexpr implies inline according to the standard, so using both is redundant

--- src/system/Time.hpp
+++ src/system/Time.hpp
@@ -20,17 +20,17 @@ namespace time {
 	static_assert(std::chrono::treat_as_floating_point<Duration::rep>::value, "Seconds duration could not be treated as a double");
 
 	inline Duration now() { return Clock::now().time_since_epoch(); }
-	inline constexpr Duration zero() { return Duration::zero(); }
+	constexpr Duration zero() { return Duration::zero(); }
 
 	// create arbitrary Durations of a specified length to aid in clarity
-	inline constexpr Duration nanoseconds(double ns)  { return std::chrono::duration<double, std::chrono::nanoseconds::period>{ ns }; }
-	inline constexpr Duration microseconds(double us) { return std::chrono::duration<double, std::chrono::microseconds::period>{ us }; }
-	inline constexpr Duration milliseconds(double ms) { return std::chrono::duration<double, std::chrono::milliseconds::period>{ ms }; }
-	inline constexpr Duration seconds(double s)       { return std::chrono::duration<double, std::chrono::seconds::period>{ s }; }
-	inline constexpr Duration minutes(double m)       { return std::chrono::duration<double, std::chrono::minutes::period>{ m }; }
-	inline constexpr Duration hours(double h)         { return std::chrono::duration<double, std::chrono::hours::period>{ h }; }
+	constexpr Duration nanoseconds(double ns)  { return std::chrono::duration<double, std::chrono::nanoseconds::period>{ ns }; }
+	constexpr Duration microseconds(double us) { return std::chrono::duration<double, std::chrono::microseconds::period>{ us }; }
+	constexpr Duration milliseconds(double ms) { return std::chrono::duration<double, std::chrono::milliseconds::period>{ ms }; }
+	constexpr Duration seconds(double s)       { return std::chrono::duration<double, std::chrono::seconds::period>{ s }; }
+	constexpr Duration minutes(double m)       { return std::chrono::duration<double, std::chrono::minutes::period>{ m }; }
+	constexpr Duration hours(double h)         { return std::chrono::duration<double, std::chrono::hours::period>{ h }; }
 	
-	inline constexpr Duration hertz(double hz)        { return Duration{ 1. / hz }; }
+	constexpr Duration hertz(double hz)        { return Duration{ 1. / hz }; }
 	
 } // ns time
 } // ns stardazed
