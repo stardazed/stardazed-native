19eb717f2358f753f3c13bf5eec88661547ca195
Arthur Langereis <puurtuur@me.com>
2014-09-17 13:42:59 +0200

    render/Buffer beginnings

---
+++ src/render/Buffer.hpp
@@ -0,0 +1,77 @@
+// ------------------------------------------------------------------
+// render::Buffer - stardazed
+// (c) 2014 by Arthur Langereis
+// ------------------------------------------------------------------
+
+#ifndef SD_RENDER_BUFFER_H
+#define SD_RENDER_BUFFER_H
+
+#include "system/Config.hpp"
+
+#include <memory>
+
+namespace stardazed {
+namespace render {
+
+
+enum class AccessLevel {
+	ReadOnly,
+	WriteOnly,
+	ReadWrite
+};
+
+
+template <typename T, AccessLevel>
+class BufferAccess;
+
+
+template <typename T>
+class Buffer {
+public:
+	virtual ~Buffer() = default;
+	
+	template <BufferAccess Level>
+	BufferAccess<T, Level> map() {
+		return { mapBufferData(Level) };
+	}
+
+private:
+	virtual T* mapBufferData(AccessLevel) = 0;
+};
+
+
+namespace detail {
+
+	class BufferPtrDeleter {
+	public:
+		void operator()(Buffer*);
+	};
+
+} // ns detail
+
+
+using BufferPtr = std::unique_ptr<>;
+
+
+template <typename T>
+class OpenGLBuffer : public Buffer<T> {
+	GLuint ubo_;
+	
+	T* bufferDataWithAccess(BufferAccess) {
+		glBindBuffer(GL_UNIFORM_BUFFER, ubo_);
+		T* blk = glMapBuffer(GL_UNIFORM_BUFFER, GL_READ_ONLY);
+	}
+	
+public:
+	OpenGLConstantBuffer() {
+		glGenBuffers(1, &ubo_);
+		glBindBuffer(GL_UNIFORM_BUFFER, ubo_);
+		glBufferData(GL_UNIFORM_BUFFER, sizeof(T), nullptr, GL_DYNAMIC_DRAW);
+	}
+};
+
+
+} // ns render
+} // ns stardazed
+
+#endif
