dfc0f7551427670d03ec2b6d676ca1a31afd3377
Arthur Langereis <puurtuur@me.com>
2014-10-14 11:40:31 +0200

    hm

--- src/render/opengl/Buffer.hpp
+++ src/render/opengl/Buffer.hpp
@@ -13,6 +13,8 @@
 #include "render/common/Mesh.hpp"
 #include "render/opengl/OpenGL.hpp"
 
+#include <cassert>
+
 
 namespace stardazed {
 namespace render {
@@ -39,6 +41,101 @@ namespace detail {
 	template <>
 	void setDefaultAttribPointer<render::Tri32>(GLuint attribIndex);
 }
+
+
+enum class BufferClientAccess {
+	None,
+	ReadOnly,
+	WriteOnly,
+	ReadWrite
+};
+
+
+enum class BufferUpdateFrequency {
+	Never,
+	Occassionally,
+	Frequently
+};
+
+
+namespace detail {
+	GLbitfield glAccessFlagsForBCA(BufferClientAccess access) {
+		switch (access) {
+			case BufferClientAccess::ReadOnly:
+				return GL_MAP_READ_BIT;
+			case BufferClientAccess::WriteOnly:
+				return GL_MAP_WRITE_BIT;
+			case BufferClientAccess::ReadWrite:
+				return GL_MAP_READ_BIT | GL_MAP_WRITE_BIT;
+			default:
+				return 0;
+		}
+	}
+	
+	GLenum glUsageHint(BufferUpdateFrequency frequency, BufferClientAccess typicalAccess) {
+		if (typicalAccess == BufferClientAccess::None) {
+			switch (frequency) {
+				case BufferUpdateFrequency::Never:
+					return GL_STATIC_COPY;
+				case BufferUpdateFrequency::Occassionally:
+					return GL_DYNAMIC_COPY;
+				case BufferUpdateFrequency::Frequently:
+					return GL_STREAM_COPY;
+			}
+		}
+
+		if (typicalAccess == BufferClientAccess::ReadWrite || typicalAccess == BufferClientAccess::WriteOnly) {
+			switch (frequency) {
+				case BufferUpdateFrequency::Never:
+					return GL_STATIC_DRAW;
+				case BufferUpdateFrequency::Occassionally:
+					return GL_DYNAMIC_DRAW;
+				case BufferUpdateFrequency::Frequently:
+					return GL_STREAM_DRAW;
+			}
+		}
+		
+		if (typicalAccess == BufferClientAccess::ReadOnly) {
+			switch (frequency) {
+				case BufferUpdateFrequency::Never:
+					return GL_STATIC_READ;
+				case BufferUpdateFrequency::Occassionally:
+					return GL_DYNAMIC_READ;
+				case BufferUpdateFrequency::Frequently:
+					return GL_STREAM_READ;
+			}
+		}
+		
+		assert(false && "Invalid frequency or access level");
+	}
+}
+
+
+
+template <typename T>
+class Buffer {
+	GLuint name_ {0};
+	size_t count_ {0};
+
+public:
+	Buffer(){
+		glGenBuffers(1, &name_);
+	}
+	
+	~Buffer() {
+		glDeleteBuffers(1, &name_);
+	}
+
+	void create(BufferUpdateFrequency frequency, BufferClientAccess access) {
+		auto usageHint = detail::glUsageHint(frequency, access);
+		glBufferData(<#GLenum target#>, 0, nullptr, usageHint);
+	}
+	
+	GLuint name() const { return name_; }
+
+	size_t size() const { return count_; }
+	size_t byteSize() const { return sizeof(T) * count_; }
+};
 
 
 template <typename T, GLenum Type = GL_ARRAY_BUFFER>
