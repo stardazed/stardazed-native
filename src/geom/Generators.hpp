fd7544ac9fd5019b679c4a4b3f9130ba740cf801
Arthur Langereis <puurtuur@me.com>
2015-01-23 22:11:39 +0100

    restore geom::gen::arc()

--- src/geom/Generators.hpp
+++ src/geom/Generators.hpp
@@ -19,8 +19,8 @@ namespace gen {
 
 render::MeshDescriptor cube(const float diameter = 1.0f);
 
-//render::MeshDescriptor arc(float minRadius, float maxRadius, int radiusSteps,
-//						   math::Angle fromAng, math::Angle toAng, int angleSteps);
+render::MeshDescriptor arc(float minRadius, float maxRadius, int radiusSteps,
+						   math::Angle fromAng, math::Angle toAng, int angleSteps);
 
 render::MeshDescriptor sphere(const int rows = 20, const int segs = 30, const float radius = 1.0f,
 							  const float sliceFrom = 0.0f, const float sliceTo = 1.0f);
