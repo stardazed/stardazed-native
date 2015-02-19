71c016cfb086fdf8a25147200260474ff93e2ac8
Arthur Langereis <puurtuur@me.com>
2015-02-19 18:33:53 +0100

    math::Plane skeleton file

---
+++ src/math/Plane.hpp
@@ -0,0 +1,26 @@
+// ------------------------------------------------------------------
+// math::Plane - stardazed
+// (c) 2015 by Arthur Langereis
+// ------------------------------------------------------------------
+
+#ifndef SD_MATH_PLANE_H
+#define SD_MATH_PLANE_H
+
+#include "system/Config.hpp"
+#include "math/Vector.hpp"
+
+namespace stardazed {
+namespace math {
+
+
+class Plane {
+	Vec3 normal;
+	float constant;
+
+public:
+	
+};
+
+
+} // ns math
+} // ns stardazed
