2a7b97c9ce1bbfe46f952dc8bc55fe72e6f77a4e
Arthur Langereis <puurtuur@me.com>
2015-02-20 11:31:17 +0100

    Some Plane methods

--- src/math/Plane.hpp
+++ src/math/Plane.hpp
@@ -14,11 +14,31 @@ namespace math {
 
 
 class Plane {
-	Vec3 normal;
-	float constant;
+	Vec3 normal_;
+	float constant_;
 
 public:
+	Plane(const Vec3& normal, float constant)
+	: normal_{normal}
+	, constant{constant}
+	{}
 	
+	constexpr bool isFacing(const Vec3& normDirection) {
+		return dot(normal_, normDirection) <= 0;
+	}
+	
+	constexpr float distanceToPoint(const Vec3& point) {
+		return dot(point, normal) + constant_;
+	}
+	
+	static constexpr Plane fromOriginAndNormal(const Vec3& origin, const Vec3& normal) {
+		return { normal, -dot(normal, origin) };
+	}
+	
+	static Plane fromTriangle(const Vec3& p1, const Vec3& p2, const Vec3& p3) {
+		auto normal = normalize(cross(p2 - p1, p3 - p1));
+		return fromOriginAndNormal(p1, normal);
+	}
 };
 
 
