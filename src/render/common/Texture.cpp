29fe0c2e18b0f2d13847fa4c7676b3d6d2926c52
Arthur Langereis <puurtuur@me.com>
2015-05-05 21:02:18 +0200

    TextureDescriptor and friends

--- src/render/common/Texture.cpp
+++ src/render/common/Texture.cpp
@@ -9,6 +9,81 @@ namespace stardazed {
 namespace render {
 
 
+TextureDescriptor makeTexDesc1D(PixelFormat pixelFormat, size32 width, UseMipMaps mipmapped) {
+	TextureDescriptor td {};
+	td.textureClass = TextureClass::Tex1D;
+	td.pixelFormat  = pixelFormat;
+	td.width = width;
+	if (mipmapped == UseMipMaps::Yes)
+		td.mipmaps = maxMipLevelsForDimension(width);
+
+	return td;
+}
+
+
+TextureDescriptor makeTexDesc1DArray(PixelFormat pixelFormat, size32 width, size32 layers, UseMipMaps mipmapped) {
+	auto td = makeTexDesc1D(pixelFormat, width, mipmapped);
+	td.layers = layers;
+	
+	return td;
+}
+
+
+TextureDescriptor makeTexDesc2D(PixelFormat pixelFormat, size32 width, size32 height, UseMipMaps mipmapped) {
+	TextureDescriptor td {};
+	td.textureClass = TextureClass::Tex2D;
+	td.pixelFormat  = pixelFormat;
+	td.width = width;
+	td.height = height;
+	if (mipmapped == UseMipMaps::Yes)
+		td.mipmaps = maxMipLevelsForDimension(math::max(width, height));
+
+	return td;
+}
+
+
+TextureDescriptor makeTexDesc2DArray(PixelFormat pixelFormat, size32 width, size32 height, size32 layers, UseMipMaps mipmapped) {
+	auto td = makeTexDesc2D(pixelFormat, width, height, mipmapped);
+	td.layers = layers;
+	
+	return td;
+}
+
+
+TextureDescriptor makeTexDesc2DMultisample(PixelFormat pixelFormat, size32 width, size32 height, size32 samples) {
+	auto td = makeTexDesc2D(pixelFormat, width, height, UseMipMaps::No);
+	td.samples = samples;
+	
+	return td;
+}
+
+
+TextureDescriptor makeTexDesc3D(PixelFormat pixelFormat, size32 width, size32 height, size32 depth, UseMipMaps mipmapped) {
+	TextureDescriptor td {};
+	td.textureClass = TextureClass::Tex3D;
+	td.pixelFormat  = pixelFormat;
+	td.width = width;
+	td.height = height;
+	td.depth = depth;
+	if (mipmapped == UseMipMaps::Yes)
+		td.mipmaps = maxMipLevelsForDimension(math::max(math::max(width, height), depth));
+
+	return td;
+}
+
+
+TextureDescriptor makeTexDescCube(PixelFormat pixelFormat, size32 dimension, UseMipMaps mipmapped) {
+	TextureDescriptor td {};
+	td.textureClass = TextureClass::TexCube;
+	td.pixelFormat  = pixelFormat;
+	td.width = dimension;
+	td.height = dimension;
+	if (mipmapped == UseMipMaps::Yes)
+		td.mipmaps = maxMipLevelsForDimension(dimension);
+
+	return td;
+}
+
 
 } // ns render
 } // ns stardazed
--- src/render/common/Texture.hpp
+++ src/render/common/Texture.hpp
@@ -7,21 +7,12 @@
 #define SD_RENDER_TEXTURE_H
 
 #include "system/Config.hpp"
+#include "math/Algorithm.hpp"
 #include "render/common/PixelFormat.hpp"
 #include "render/common/PixelBuffer.hpp"
 
 namespace stardazed {
 namespace render {
-
-
-enum class CubeMapFace : size32 {
-	PosX,
-	NegX,
-	PosY,
-	NegY,
-	PosZ,
-	NegZ
-};
 
 
 enum class TextureClass : uint32 {
@@ -32,11 +23,55 @@ enum class TextureClass : uint32 {
 };
 
 
-struct TextureDescriptor {
-	TextureClass textureClass;
-	PixelFormat pixelFormat;
-	size32 width, height, depth, layers, mipmaps, samples;
+enum class TextureUsageHint : uint32 {
+	Normal,
+	RenderTargetOnly
 };
+
+
+enum class UseMipMaps : bool32 {
+	No = 0,
+	Yes = 1
+};
+
+
+// the numerical constant for each face is the layer index in the cubemap
+enum class CubeMapFace : size32 {
+	PosX,
+	NegX,
+	PosY,
+	NegY,
+	PosZ,
+	NegZ
+};
+
+
+struct TextureDescriptor {
+	TextureClass textureClass = TextureClass::Tex2D;
+	PixelFormat pixelFormat = PixelFormat::None;
+	TextureUsageHint usageHint = TextureUsageHint::Normal;
+	size32 width = 1, height = 1, depth = 1;
+	size32 layers = 1, mipmaps = 1, samples = 1;
+};
+
+
+inline size32 maxMipLevelsForDimension(size32 dim) {
+	return 1 + (size32)std::floor(std::log2((float)dim));
+}
+
+
+// -- helpers functions to quickly setup common texture descriptors
+
+TextureDescriptor makeTexDesc1D(PixelFormat pixelFormat, size32 width, UseMipMaps mipmapped);
+TextureDescriptor makeTexDesc1DArray(PixelFormat pixelFormat, size32 width, size32 layers, UseMipMaps mipmapped);
+
+TextureDescriptor makeTexDesc2D(PixelFormat pixelFormat, size32 width, size32 height, UseMipMaps mipmapped);
+TextureDescriptor makeTexDesc2DArray(PixelFormat pixelFormat, size32 width, size32 height, size32 layers, UseMipMaps mipmapped);
+TextureDescriptor makeTexDesc2DMultisample(PixelFormat pixelFormat, size32 width, size32 height, size32 samples);
+
+TextureDescriptor makeTexDesc3D(PixelFormat pixelFormat, size32 width, size32 height, size32 depth, UseMipMaps mipmapped);
+
+TextureDescriptor makeTexDescCube(PixelFormat pixelFormat, size32 dimension, UseMipMaps mipmapped);
 
 
 } // ns render
