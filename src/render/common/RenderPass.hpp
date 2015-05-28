06d0c4576b0aa781ac2663f3e6beeeb1655b6ca2
Arthur Langereis <puurtuur@me.com>
2015-05-28 18:15:05 +0200

    more renderpass stuff

--- src/render/common/RenderPass.hpp
+++ src/render/common/RenderPass.hpp
@@ -7,7 +7,7 @@
 #define SD_RENDER_RENDERPASS_H
 
 #include "system/Config.hpp"
-
+#include "math/Vector.hpp"
 #include <array>
 
 namespace stardazed {
@@ -15,27 +15,46 @@ namespace render {
 
 
 class Texture;
-class Shader;
-class Sampler;
 
 
-struct TextureBinding {
-	Texture* tex = nullptr;
-	Sampler* sampler = nullptr;
+enum class AttachmentLoadAction {
+	None,
+	Load,
+	Clear
 };
 
 
-struct RenderPass {
-	std::array<TextureBinding, 16> textures;
+enum class AttachmentStoreAction {
+	None,
+	Store
 };
 
 
-struct Pipeline {
-	Shader* vertexShader = nullptr;
-	Shader* geometryShader = nullptr;
-	Shader* fragmentShader = nullptr;
+struct AttachmentDescriptor {
+	Texture* texture = nullptr;
+	uint32 level = 0;	// mipmap
+	uint32 layer = 0;	// array textures only
+	uint32 depth = 0;	// 3D textures only
+	
+	AttachmentLoadAction loadAction = AttachmentLoadAction::None;
+	AttachmentStoreAction storeAction = AttachmentStoreAction::None;
+};
 
-	PositionedAttributeList vertex
+
+struct RenderPassDescriptor {
+	std::array<AttachmentDescriptor, 8> colourAttachments;
+	std::array<math::Vec4, 8> clearColours;
+
+	AttachmentDescriptor depthAttachment;
+	float clearDepth = 1.0f;
+
+	AttachmentDescriptor stencilAttachment;
+	uint32 clearStencil = 0;
+
+	RenderPassDescriptor() {
+		for (auto& colour : clearColours)
+			colour = { 0, 0, 0, 1 };
+	}
 };
 
 
