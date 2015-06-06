c7e3792ae7d28e894520c0b011c2b8903734e6e6
Arthur Langereis <puurtuur@me.com>
2015-06-06 22:59:56 +0200

    apply and restore colour write masks and blending modes in Pipeline

--- src/render/opengl/Pipeline.cpp
+++ src/render/opengl/Pipeline.cpp
@@ -9,6 +9,38 @@ namespace stardazed {
 namespace render {
 
 
+constexpr GLenum glBlendEqForBlendOperation(BlendOperation op) {
+	switch (op) {
+		case BlendOperation::Add: return GL_FUNC_ADD;
+		case BlendOperation::Subtract: return GL_FUNC_SUBTRACT;
+		case BlendOperation::ReverseSubtract: GL_FUNC_REVERSE_SUBTRACT;
+		case BlendOperation::Min: return GL_MIN;
+		case BlendOperation::Max: return GL_MAX;
+	}
+}
+
+
+constexpr GLenum glBlendFuncForBlendFactor(BlendFactor factor) {
+	switch (factor) {
+		case BlendFactor::Zero: return GL_ZERO;
+		case BlendFactor::One: return GL_ONE;
+		case BlendFactor::SourceColour: return GL_SRC_COLOR;
+		case BlendFactor::OneMinusSourceColour: GL_ONE_MINUS_SRC_COLOR;
+		case BlendFactor::DestColour: return GL_DST_COLOR;
+		case BlendFactor::OneMinusDestColour: return GL_ONE_MINUS_DST_COLOR;
+		case BlendFactor::SourceAlpha: return GL_SRC_ALPHA;
+		case BlendFactor::OneMinusSourceAlpha: return GL_ONE_MINUS_SRC_ALPHA;
+		case BlendFactor::SourceAlphaSaturated: return GL_SRC_ALPHA_SATURATE;
+		case BlendFactor::DestAlpha: return GL_DST_ALPHA;
+		case BlendFactor::OneMinusDestAlpha: return GL_ONE_MINUS_DST_ALPHA;
+		case BlendFactor::ConstantColour: return GL_CONSTANT_COLOR;
+		case BlendFactor::OneMinusConstantColour: return GL_ONE_MINUS_CONSTANT_COLOR;
+		case BlendFactor::ConstantAlpha: return GL_CONSTANT_ALPHA;
+		case BlendFactor::OneMinusConstantAlpha: return GL_ONE_MINUS_CONSTANT_ALPHA;
+	}
+}
+
+
 Pipeline::Pipeline(const PipelineDescriptor& descriptor)
 : descriptor_(descriptor)
 , vertexLayout_(descriptor.vertexAttributes)
@@ -45,17 +77,59 @@ Pipeline::~Pipeline() {
 
 void Pipeline::bind() const {
 	glBindProgramPipeline(name());
-	if (! descriptor_.rasterizerEnabled)
+
+	if (! descriptor_.rasterizerEnabled) {
 		glEnable(GL_RASTERIZER_DISCARD);
-	
-	// FIXME: set all the blending modes, etc
+	}
+	else {
+		// -- apply write masks and blending modes for enabled colour attachments
+		uint32 attIndex = 0;
+		bool useBlending = false;
+
+		for (auto& caDesc : descriptor_.colourAttachments) {
+			if (caDesc.pixelFormat != PixelFormat::None) {
+				glColorMaski(attIndex, caDesc.writeMask.red, caDesc.writeMask.green, caDesc.writeMask.blue, caDesc.writeMask.alpha);
+
+				if (caDesc.blending.enabled) {
+					useBlending = true;
+					auto rgbEq = glBlendEqForBlendOperation(caDesc.blending.rgbBlendOp);
+					auto alphaEq = glBlendEqForBlendOperation(caDesc.blending.alphaBlendOp);
+					glBlendEquationSeparatei(attIndex, rgbEq, alphaEq);
+
+					auto rgbSrcFn = glBlendFuncForBlendFactor(caDesc.blending.sourceRGBFactor);
+					auto alphaSrcFn = glBlendFuncForBlendFactor(caDesc.blending.sourceAlphaFactor);
+					auto rgbDestFn = glBlendFuncForBlendFactor(caDesc.blending.destRGBFactor);
+					auto alphaDestFn = glBlendFuncForBlendFactor(caDesc.blending.destAlphaFactor);
+					glBlendFuncSeparatei(attIndex, rgbSrcFn, rgbDestFn, alphaSrcFn, alphaDestFn);
+				}
+			}
+			
+			++attIndex;
+		}
+
+		// -- in GL, blending is a global state
+		if (useBlending)
+			glEnable(GL_BLEND);
+	}
 }
 
 
 void Pipeline::unbind() const {
 	glBindProgramPipeline(0);
-	if (! descriptor_.rasterizerEnabled)
+
+	// FIXME: since these are only set in Pipeline, can we move
+	// leave these out and just set/unset all the values in bind()?
+
+	if (! descriptor_.rasterizerEnabled) {
 		glDisable(GL_RASTERIZER_DISCARD);
+	}
+	else {
+		// -- restore all write masks and blending modes to defaults
+		glColorMask(GL_TRUE, GL_TRUE, GL_TRUE, GL_TRUE);
+		glDisable(GL_BLEND);
+		glBlendEquation(GL_FUNC_ADD);
+		glBlendFunc(GL_ONE, GL_ZERO);
+	}
 }
 
 
