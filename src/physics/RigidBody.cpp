c7f3b76d404bd5053b561a2b5751ae515899edb5
Arthur Langereis <puurtuur@me.com>
2015-08-11 20:09:37 +0200

    WIP: combine PhysicsState and RigidBody into RigidBodyManager component

--- src/physics/RigidBody.cpp
+++ src/physics/RigidBody.cpp
@@ -9,25 +9,44 @@ namespace stardazed {
 namespace physics {
 
 
-RigidBody::RigidBody(Transform& linkedTransform, float mass, float angularInertia)
-: previousTransform_(linkedTransform)
-, previous_{ previousTransform_, mass, angularInertia }
-, current_ { linkedTransform, mass, angularInertia }
-{}
+//	void PhysicsState::recalcSecondaryValues() {
+//		velocity_ = momentum * oneOverMass_;
+//		angularVelocity_ = angularMomentum * oneOverAngularInertia_;
+//		math::normalizeInPlace(transform.rotation);
+//		spin_ = 0.5 * math::Quat{angularVelocity_, 0} * transform.rotation;
+//	}
+//	
+//	
+//	void PhysicsState::copyPrimaryAndSecondaryValuesFrom(const PhysicsState& other) {
+//		transform = other.transform;
+//		momentum = other.momentum;
+//		angularMomentum = other.angularMomentum;
+//		
+//		velocity_ = other.velocity_;
+//		angularVelocity_ = other.angularVelocity_;
+//		spin_ = other.spin_;
+//	}
 
 
-void RigidBody::update(Time t, Time dt) {
-	previous_.copyPrimaryAndSecondaryValuesFrom(current_);
-	integrate(current_, t, dt);
-	userForce = {0, 0, 0};
-	userTorque = {0, 0, 0};
-}
-
-
-void RigidBody::calcForces(const PhysicsState& state, const Time /*globalTime*/, math::Vec3& outForce, math::Vec3& outTorque) const {
-	outForce  = userForce - state.momentum * .5;
-	outTorque = userTorque - state.angularMomentum * .5;
-}
+//RigidBody::RigidBody(Transform& linkedTransform, float mass, float angularInertia)
+//: previousTransform_(linkedTransform)
+//, previous_{ previousTransform_, mass, angularInertia }
+//, current_ { linkedTransform, mass, angularInertia }
+//{}
+//
+//
+//void RigidBody::update(Time t, Time dt) {
+//	previous_.copyPrimaryAndSecondaryValuesFrom(current_);
+//	integrate(current_, t, dt);
+//	userForce = {0, 0, 0};
+//	userTorque = {0, 0, 0};
+//}
+//
+//
+//void RigidBody::calcForces(const PhysicsState& state, const Time /*globalTime*/, math::Vec3& outForce, math::Vec3& outTorque) const {
+//	outForce  = userForce - state.momentum * .5;
+//	outTorque = userTorque - state.angularMomentum * .5;
+//}
 
 
 } // ns physics
--- src/physics/RigidBody.hpp
+++ src/physics/RigidBody.hpp
@@ -8,18 +8,91 @@
 
 #include "system/Config.hpp"
 #include "system/Time.hpp"
-#include "physics/Integratable.hpp"
+#include "container/MultiArrayBuffer.hpp"
+#include "container/HashMap.hpp"
+#include "scene/Entity.hpp"
+#include "scene/Transform.hpp"
 
 namespace stardazed {
 namespace physics {
 
 
-struct Environment {
-	math::Vec3 gravity = earthGravity();
-	float airDensity = earthSeaLevelAirDensity();
+class RigidBodyManager {
+public:
+	using Instance = scene::Instance<RigidBodyManager>;
+
+private:
+	scene::TransformComponent& transform_;
+
+	container::MultiArrayBuffer<
+		// constant
+		float, // mass / inertia
+		float, // inverseMass
+		float, // angularInertia
+		float, // inverseAngInertia
+
+		// secondary
+		math::Vec3, // velocity
+		math::Vec3, // angularVelocity
+		math::Quat, // spin
+
+		// primary
+		scene::TransformComponent::Instance, // linkedTransformInstance
+		math::Vec3, // momentum
+		math::Vec3, // angularMomentum
+	
+		// previous state
+		math::Vec3, // previousPosition
+		math::Quat, // previousRotation
+		math::Vec3  // previousVelocity
+	> instanceData_;
+	
+	HashMap<scene::Entity, Instance> entityMap_;
+	
+	enum class InstField : uint {
+		Mass,
+		InverseMass,
+		AngularInertia,
+		InverseAngInertia,
+		
+		Velocity,
+		AngularVelocity,
+		Spin,
+		
+		Transform,
+		Momentum,
+		AngularMomentum,
+		
+		PreviousPosition,
+		PreviousRotation,
+		PreviousVelocity
+	};
+	
+	template <InstField F>
+	auto basePtr() const {
+		return instanceData_.template elementsBasePtr<(uint)F>();
+	}
+
+public:
+	RigidBodyManager(memory::Allocator&, scene::TransformComponent&);
+
+	Instance create(scene::Entity, float mass, float angularInertia);
+	
+	// -- single instance access
+	float mass(Instance h) const { return *(basePtr<InstField::Mass>() + h.ref); }
+	float angularInertia(Instance h) const { return *(basePtr<InstField::AngularInertia>() + h.ref); }
+	
+	scene::TransformComponent::Instance transform(Instance h) const { return *(basePtr<InstField::Transform>() + h.ref); }
+	math::Vec3& momentum(Instance h) { return *(basePtr<InstField::Momentum>() + h.ref); }
+	math::Vec3& angularMomentum(Instance h) { return *(basePtr<InstField::AngularMomentum>() + h.ref); }
+	
+	const math::Vec3& velocity(Instance h) const { return *(basePtr<InstField::Velocity>() + h.ref); }
+	const math::Quat& spin(Instance h) const { return *(basePtr<InstField::Spin>() + h.ref); }
+	const math::Vec3& angularVelocity(Instance h) const { return *(basePtr<InstField::AngularVelocity>() + h.ref); }
 };
 
 
+/*
 class RigidBody : public Integratable<RigidBody> {
 	Transform previousTransform_;
 	PhysicsState previous_, current_;
@@ -47,6 +120,7 @@ public:
 	// -- integration
 	void calcForces(const PhysicsState&, const Time globalTime, math::Vec3& outForce, math::Vec3& outTorque) const;
 };
+*/
 
 
 } // ns physics
